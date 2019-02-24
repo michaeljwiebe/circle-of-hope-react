fa650e124704e1307b715607d6eb1b3688d8510a
Michael J. Wiebe <wiebe.michael.j@gmail.com>
2019-02-24 10:52:28 -0500

    moved songs to listAndSearch

--- src/App.js
+++ src/App.js
@@ -23,12 +23,12 @@ class App extends Component {
     
     render () {
         console.log('this.props', this.props)
-        let { selectedSongId } = this.props
-        let song = songs.find((song) => song.id === selectedSongId)
-        let songRender = null
-        if (selectedSongId > -1) {
-            songRender = <Song songData={song} />
-        }
+        // let { selectedSongId } = this.props
+        // let song = songs.find((song) => song.id === selectedSongId)
+        // let songRender = null
+        // if (selectedSongId > -1) {
+        //     songRender = <Song songData={song} />
+        // }
         return (
             <div style={ appContainerStyles }>
                 <Header />
@@ -37,10 +37,10 @@ class App extends Component {
                     <div style={ listAndSongContainer }>
                         <ListAndSearch 
                             {...this.props} 
-                            songs={songs} 
+                            // songs={songs} 
                             onSearch={this.handleUpdateSearch} 
                             />
-                        { songRender }
+                        {/* { songRender } */}
                     </div>
                 </div>
             </div>
@@ -84,32 +84,32 @@ const mapDispatchToProps = {
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
 
-let songs = [{
-    title: 'one',
-    artist: 'Alex',
-    lyrics: 'la fa mi la so fa so la fa fa fa la fa mi la so fa so la fa fa fa la fa mi la so fa so la fa fa fa la fa mi la so fa so la fa fa fa la fa mi la so fa so la fa fa fa',
-    id: '1'
-}, {
-    title: 'two',
-    artist: 'Ben',
-    lyrics: 'Great God, the giver of all good, accept our thanks and bless this food, grace health and strength to us afford, through Jesus Christ our risen lord',
-    id: '2'
-}, {
-    title: 'three',
-    artist: 'Psalters',
-    lyrics: 'Come now and join the feast, right here in the belly of the beast! Cops and soldiers you can come too, just lay down your guns and come on through, rich people get rid of your stuff and poor people there will be enough',
-    id: '33'
-}, {
-    title: 'four',
-    artist: 'Circle - Fishtown',
-    lyrics: 'Blow ye the trumpet blow, The gladly solemn sound Let all the nations know, To earth’s remotest bound, Chorus: The year of Jubilee is come; Return, ye ransomed sinners, home. (end chorus) Extol the Lamb of God, The all- atoning Lamb; Redemption through his blood, Throughout the world proclaim. (Chorus) The Gospel trumpet hear, The news of heav’nly grace; And saved from earth appear, Before your Savior’s face.',
-    id: '44'
-}, {
-    title: 'five',
-    artist: 'Sarah',
-    lyrics: 'My Christian friends, in bonds of love, Whose hearts in sweetest union join, Your friendship’s like a drawing band, Yet we must take the parting hand. Your company’s sweet, your union dear, Your words delightful to my ear; Yet when I see that we must part You draw like cords around my heart.',
-    id: '55'
-}]
+// let songs = [{
+//     title: 'one',
+//     artist: 'Alex',
+//     lyrics: 'la fa mi la so fa so la fa fa fa la fa mi la so fa so la fa fa fa la fa mi la so fa so la fa fa fa la fa mi la so fa so la fa fa fa la fa mi la so fa so la fa fa fa',
+//     id: '1'
+// }, {
+//     title: 'two',
+//     artist: 'Ben',
+//     lyrics: 'Great God, the giver of all good, accept our thanks and bless this food, grace health and strength to us afford, through Jesus Christ our risen lord',
+//     id: '2'
+// }, {
+//     title: 'three',
+//     artist: 'Psalters',
+//     lyrics: 'Come now and join the feast, right here in the belly of the beast! Cops and soldiers you can come too, just lay down your guns and come on through, rich people get rid of your stuff and poor people there will be enough',
+//     id: '33'
+// }, {
+//     title: 'four',
+//     artist: 'Circle - Fishtown',
+//     lyrics: 'Blow ye the trumpet blow, The gladly solemn sound Let all the nations know, To earth’s remotest bound, Chorus: The year of Jubilee is come; Return, ye ransomed sinners, home. (end chorus) Extol the Lamb of God, The all- atoning Lamb; Redemption through his blood, Throughout the world proclaim. (Chorus) The Gospel trumpet hear, The news of heav’nly grace; And saved from earth appear, Before your Savior’s face.',
+//     id: '44'
+// }, {
+//     title: 'five',
+//     artist: 'Sarah',
+//     lyrics: 'My Christian friends, in bonds of love, Whose hearts in sweetest union join, Your friendship’s like a drawing band, Yet we must take the parting hand. Your company’s sweet, your union dear, Your words delightful to my ear; Yet when I see that we must part You draw like cords around my heart.',
+//     id: '55'
+// }]
 
     // render () {
     //     let { selectedSongId } = this.props
--- src/components/ListAndSearch.js
+++ src/components/ListAndSearch.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import ListItem from './ListItem'
 // due to list filtering functionality, i think it makes sense to render SearchBar here
 import SearchBar from './SearchBar'
+import Song from './Song'
 
 import { updateSearch } from '../redux/actions'
 
@@ -17,7 +18,8 @@ class ListAndSearch extends Component {
     }
 
     renderList() {
-        let songs = this.props.songs
+        // let songs = this.props.songs
+        let songs = this.songs
         if (this.props.searchText) songs = this.state.filteredList
         return songs.map(song => {
             return <ListItem {...song} key={song.id} />
@@ -52,14 +54,48 @@ class ListAndSearch extends Component {
 
     render() {
         let list = this.renderList()
+        let { selectedSongId } = this.props
+        let song = this.songs.find((song) => song.id === selectedSongId)
+        let songRender = null
+        if (selectedSongId > -1) {
+            songRender = <Song songData={song} />
+        }
         return (
             <div>
                 <SearchBar />
                 {/* <SearchBar onChange={this.onSearch.bind(this)}/> */}
                 <div>{list}</div>
+                { songRender }
             </div>
         )
     }
+
+    songs = [{
+        title: 'one',
+        artist: 'Alex',
+        lyrics: 'la fa mi la so fa so la fa fa fa la fa mi la so fa so la fa fa fa la fa mi la so fa so la fa fa fa la fa mi la so fa so la fa fa fa la fa mi la so fa so la fa fa fa',
+        id: '1'
+    }, {
+        title: 'two',
+        artist: 'Ben',
+        lyrics: 'Great God, the giver of all good, accept our thanks and bless this food, grace health and strength to us afford, through Jesus Christ our risen lord',
+        id: '2'
+    }, {
+        title: 'three',
+        artist: 'Psalters',
+        lyrics: 'Come now and join the feast, right here in the belly of the beast! Cops and soldiers you can come too, just lay down your guns and come on through, rich people get rid of your stuff and poor people there will be enough',
+        id: '33'
+    }, {
+        title: 'four',
+        artist: 'Circle - Fishtown',
+        lyrics: 'Blow ye the trumpet blow, The gladly solemn sound Let all the nations know, To earth’s remotest bound, Chorus: The year of Jubilee is come; Return, ye ransomed sinners, home. (end chorus) Extol the Lamb of God, The all- atoning Lamb; Redemption through his blood, Throughout the world proclaim. (Chorus) The Gospel trumpet hear, The news of heav’nly grace; And saved from earth appear, Before your Savior’s face.',
+        id: '44'
+    }, {
+        title: 'five',
+        artist: 'Sarah',
+        lyrics: 'My Christian friends, in bonds of love, Whose hearts in sweetest union join, Your friendship’s like a drawing band, Yet we must take the parting hand. Your company’s sweet, your union dear, Your words delightful to my ear; Yet when I see that we must part You draw like cords around my heart.',
+        id: '55'
+    }]
 }
 
 const mapStateToProps = (state, ownProps) => {
@@ -76,3 +112,4 @@ const mapDispatchToProps = {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ListAndSearch)
+
